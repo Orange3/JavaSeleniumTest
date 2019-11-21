8d3808876462aaf98aa3987aeb852eba5984171e
Orange3 <orange9106196@gmail.com>
2019-11-21 23:41:31 +0200

    Add tests Add fromResponse page Changed form page

--- src/test/java/pages/FormPage.java
+++ src/test/java/pages/FormPage.java
@@ -15,6 +15,11 @@ public class FormPage {
         EMAIL, AGE, NAME, MOOD
     }
 
+    public FormPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
     @FindBy(xpath = "//div[@class='freebirdFormviewerViewHeaderHeader exportHeader']")
     private WebElement titleField;
 
@@ -30,13 +35,14 @@ public class FormPage {
     @FindBy(css = "div[role='list'] div[role='checkbox']")
     private List<WebElement> checkBoxes;
 
+    @FindBy(css = "input[class='quantumWizTextinputSimpleinputInput exportInput']")
+    private WebElement moodOtherOptionField;
+
     @FindBy(css = "div[class='freebirdFormviewerViewItemsItemErrorMessage']" )
     private List<WebElement> errorMessages; //error messages for each field
 
-    public FormPage(WebDriver driver){
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
+    @FindBy(css = "div[class='freebirdFormviewerViewNavigationButtonsAndProgress']  div[role='button']")
+    private WebElement submitButton;
 
     //filling email field
     public void enterEmail(String email){
@@ -49,8 +55,11 @@ public class FormPage {
     //filling age field
     public void enterAge(String month, String day, String year){
         ageField.click();
+        ageField.click();
         ageField.clear();
-        ageField.sendKeys(month+day+year);
+        ageField.sendKeys(month);
+        ageField.sendKeys(day);
+        ageField.sendKeys(year);
         titleField.click();
     }
 
@@ -65,10 +74,44 @@ public class FormPage {
         yourNameField.sendKeys(name);
     }
 
-    public WebElement getCheckbox(int id){
-        return checkBoxes.get(id);
+    public List<WebElement> getCheckBoxesList(){
+        return checkBoxes;
     }
 
+    //Check that checkbox/radio button is checked
+    public boolean isOptionSelected(WebElement element){
+        if(element.getAttribute("aria-checked").equals("true"))
+            return true;
+        else return false;
+    }
+
+    public void enterTextIntoMoodOtherOption(String text){
+        moodOtherOptionField.click();
+        moodOtherOptionField.clear();
+        moodOtherOptionField.sendKeys(text);
+    }
+
+    //Uncheck options
+    public void resetCheckBoxes(List<WebElement> options){
+        for(int i=0; i<options.size(); i++){
+            if (isOptionSelected(options.get(i)))
+                options.get(i).click();
+        }
+    }
+
+    //Method for calculating selected checkboxes
+    public int countCheckedOptions(List<WebElement> options){
+        int checkedOptionsCounter = 0;
+        for(int i=0; i<options.size(); i++){
+            if (isOptionSelected(options.get(i))) checkedOptionsCounter++;
+        }
+        return checkedOptionsCounter;
+    }
+
+    public void clickSubmit(){
+        submitButton.click();
+    }
+    //Return of error message for specified field
     public WebElement getErrorMessage(FormType field){
         switch (field){
             case EMAIL: return errorMessages.get(0);
--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -3,10 +3,13 @@ package test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import pages.FormPage;
+import pages.FormResponsePage;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,7 +19,9 @@ import java.util.concurrent.TimeUnit;
 public class TestClass {
     private static ChromeDriverService service;
     public static WebDriver driver;
+    public static WebDriverWait wait;
     FormPage formPage;
+    FormResponsePage formResponsePage;
 
     @BeforeSuite
     public void createAndStartService() throws IOException {
@@ -28,8 +33,10 @@ public class TestClass {
         driver = new ChromeDriver(service);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
         driver.get("https://goo.gl/forms/t16Uov7ZHXCrB2ZE2");
         formPage = new FormPage(driver);
+        formResponsePage = new FormResponsePage(driver);
     }
 
     @Test(description = "Check email form for a correctness of an entered data", groups = "form", priority = 1)
@@ -46,8 +53,7 @@ public class TestClass {
         Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.EMAIL).isDisplayed());
 
         formPage.enterEmail(".username@yahoo.com");
-        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.EMAIL).isDisplayed()); //Error is expected, test is failed. Message should be displayed, but it isn't
-
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.EMAIL).isDisplayed()); //test will be failed. Message should be displayed but it isn't
     }
 
     @Test(description = "Check age form for a correctness of an entered data", groups = "form", priority = 2)
@@ -72,15 +78,15 @@ public class TestClass {
 
         month = "2"; day = "30"; year="1999";
         formPage.enterAge(month,day,year);
-        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test is failed. Incorrect date is present. Error Message should be displayed, but it isn't
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test will be failed. Incorrect date is present. Error Message should be displayed but it isn't
 
         month = "31"; day="12"; year="012345";
         formPage.enterAge(month,day,year);
-        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test is failed. Incorrect date is present. Error Message should be displayed, but it isn't
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test will be failed. Incorrect date is present. Error Message should be displayed but it isn't
 
         month = "as"; day="ds"; year="012345";
         formPage.enterAge(month,day,year);
-        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test is failed. Incorrect date is present. Error Message should be displayed, but it isn't
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed()); //test will be failed. Incorrect date is present. Error Message should be displayed but it isn't
     }
 
     @Test(description = "Check name form for a correctness of an entered data", groups = "form", priority = 3)
@@ -90,7 +96,7 @@ public class TestClass {
         Assert.assertEquals(false, formPage.getErrorMessage(FormPage.FormType.NAME).isDisplayed());
 
         formPage.enterYourNameField("Cairro Worthington"); //string.length = 18
-        Assert.assertEquals(false, formPage.getErrorMessage(FormPage.FormType.NAME).isDisplayed()); //test is failed. Incorrect message length is set.
+        Assert.assertEquals(false, formPage.getErrorMessage(FormPage.FormType.NAME).isDisplayed()); //test will be failed. Incorrect message length is set.
 
         //Check invalid data
         formPage.enterYourNameField("Hubert Blaine Wolfeschlegelsteinhausenbergerdorff");
@@ -102,11 +108,54 @@ public class TestClass {
         formPage.enterYourNameField("123#$%asd");
         Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.NAME).isDisplayed()); //test is failed. Special symbols are not detected.
     }
-    @Test(description = "Check name form for a correctness of an entered data", groups = "form", priority = 4)
-    public void moodFieldsTest(){
 
-        formPage.getCheckbox(0).click();
-        Assert.assertEquals(true, formPage.getCheckbox(0).isSelected());
+    @Test(description = "Verify a correctness of checked options in the mood form", groups = "form", priority = 4)
+    public void moodFieldsTest(){
+        //Valid data
+        //Check 1 selected option; Error message shouldn't be displayed
+        formPage.getCheckBoxesList().get(0).click();
+        Assert.assertEquals(formPage.countCheckedOptions(formPage.getCheckBoxesList()), 1);
+        Assert.assertEquals(false, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+
+        formPage.resetCheckBoxes(formPage.getCheckBoxesList());
+
+        //Check "another" selected option and empty text field. Error message should be displayed
+        formPage.getCheckBoxesList().get(4).click();
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+        //Check filled text-form and error message
+        formPage.enterTextIntoMoodOtherOption("Great!");
+        Assert.assertEquals(false, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+
+        //Invalid data
+        //Check appearance of error for not selected options
+        formPage.resetCheckBoxes(formPage.getCheckBoxesList());
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+
+        //Check several selected options
+        formPage.getCheckBoxesList().get(1).click();
+        formPage.getCheckBoxesList().get(3).click();
+        //Test will be failed. Error message is not displayed. Data can be sent. Radio button should be used instead of checkboxes
+        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+    }
+
+    @Test(description = "Verify that a data can be sent", groups = "form", priority = 5)
+    public void submitDataTest(){
+        //Submit with empty data
+//        driver.get("https://goo.gl/forms/t16Uov7ZHXCrB2ZE2");
+//        formPage.clickSubmit();
+//        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.EMAIL).isDisplayed());
+//        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.AGE).isDisplayed());
+//        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.NAME).isDisplayed());
+//        Assert.assertEquals(true, formPage.getErrorMessage(FormPage.FormType.MOOD).isDisplayed());
+
+        //Submit with valid data
+        formPage.enterEmail("user_name@domain.com");
+        formPage.enterAge("11","20","1995");
+        formPage.enterYourNameField("Arley Gilmore");
+        formPage.getCheckBoxesList().get(0).click();
+        formPage.clickSubmit();
+        wait.until(ExpectedConditions.visibilityOf(formResponsePage.getSendAgainLink()));
+
     }
 
     @AfterClass
@@ -114,5 +163,7 @@ public class TestClass {
         //service.stop();
         driver.quit();
     }
+
+
 }
 
